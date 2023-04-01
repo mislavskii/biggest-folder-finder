29cb4f8c4ded1ec3e22c00b456f0f42d0cd7eefc
mislavskii <jornjat@gmail.com>
2023-04-01 16:55:28 +0700

    human readable and reverse implemented

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,15 @@
 import java.io.File;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
 import java.util.concurrent.ForkJoinPool;
 
 public class Main {
+    static String[] UNITS = {"b", "Kb", "Mb", "Gb", "Tb"};
+
     public static void main(String[] args){
 
         System.out.println("Hello world of sizes!\n");
-
-//        MyThread thread = new MyThread(1);
-//        MyThread thread2 = new MyThread(2);
-//
-//        thread.start();
-//        thread2.start();
-
 
         String folderPath = "D:/User/Learn";
         File file = new File(folderPath);
@@ -22,13 +19,16 @@ public class Main {
         FolderSizeMeter meter = new FolderSizeMeter(file);
         ForkJoinPool pool = new ForkJoinPool();
         long size = pool.invoke(meter);
-        System.out.println("Total size: " + size / Math.pow(1024, 2) + " Mb");
+        System.out.println("Total size: " + size);
+        System.out.println("that is " + getHumanReadableSize(size));
         long duration = System.currentTimeMillis() - start;
         System.out.println("Elapsed: " + duration + " ms");
+        System.out.println(getSizeFromHumanReadable(getHumanReadableSize(size)));
+
+        System.out.println(getSizeFromHumanReadable("235Kb"));
 
     }
     public static long getFolderSize(File folder) {
-//        String[] units = {"b", "Kb", "Mb", "Gb"};
         if (folder.isFile()) {
             return folder.length();
         }
@@ -41,6 +41,28 @@ public class Main {
         }
         return total;
     }
+
+    public static String getHumanReadableSize(long size) {
+        int power = (int) (Math.log(size) / Math.log(1024));
+        long value = Math.round(size / Math.pow(1024, power));
+        return value + UNITS[power];
+    }
+
+    public static long getSizeFromHumanReadable(String size) {
+        int numericLength = 0;
+        for (char c : size.toCharArray()) {
+            if (Character.isDigit(c)) {
+                numericLength += 1;
+            }  else {
+                break;
+            }
+        }
+        String numericPart = size.substring(0, numericLength);
+        String unit = size.substring(numericLength);
+        int power = Arrays.asList(UNITS).indexOf(unit);
+        return (long) (Integer.parseInt(numericPart) * Math.pow(1024, power));
+    }
+
 }
 
 //        Set<Object> keys = System.getProperties().keySet();
